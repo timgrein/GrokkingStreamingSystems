a5d26703e462527caf07f9375a36a95fdb4a0925
Tim Grein <tim@4greins.de>
2023-06-11 11:35:43 +0200

    Autogenerate vehicle events.

--- src/main/java/com/streamwork/ch02/job/SensorReader.java
+++ src/main/java/com/streamwork/ch02/job/SensorReader.java
@@ -2,13 +2,16 @@ package com.streamwork.ch02.job;
 
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.streamwork.ch02.api.Event;
 import com.streamwork.ch02.api.Source;
 
 class SensorReader extends Source {
   private final BufferedReader reader;
+  private final List<String> vehicleTypes = List.of("car", "truck", "bike");
 
   public SensorReader(String name, int port) {
     super(name);
@@ -19,17 +22,20 @@ class SensorReader extends Source {
   @Override
   public void getEvents(List<Event> eventCollector) {
     try {
-      String vehicle = reader.readLine();
-      if (vehicle == null) {
-        // Exit when user closes the server.
-        System.exit(0);
-      }
-      eventCollector.add(new VehicleEvent(vehicle));
-      System.out.println("");  // An empty line before logging new events
-      System.out.println("SensorReader --> " + vehicle);
-    } catch (IOException e) {
-      System.out.println("Failed to read input: " + e);
+      String randomVehicleType = vehicleTypes.get(getRandomNumberInRange(0, vehicleTypes.size() - 1));
+      Thread.sleep(getRandomNumberInRange(1, 3) * 1000L);
+      eventCollector.add(new VehicleEvent(randomVehicleType));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
+
+  public int getRandomNumberInRange(int lower, int upper) {
+    if (lower > upper) {
+      throw new IllegalArgumentException("Upper bound must be greater than lower bound");
+    }
+
+    return new Random().nextInt(upper - lower + 1) + lower;
   }
 
   private BufferedReader setupSocketReader(int port) {

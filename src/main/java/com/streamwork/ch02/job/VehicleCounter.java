382a767ff0e9510f5d6983724b2e72975cc9bafe
Tim Grein <tim@4greins.de>
2023-06-11 11:41:23 +0200

    Print total amount of fees.

--- src/main/java/com/streamwork/ch02/job/VehicleCounter.java
+++ src/main/java/com/streamwork/ch02/job/VehicleCounter.java
@@ -10,18 +10,26 @@ import com.streamwork.ch02.api.Event;
 import com.streamwork.ch02.api.Operator;
 
 class VehicleCounter extends Operator {
-  private final Map<String, Integer> countMap = new HashMap<String, Integer>();
+  private final Map<String, Integer> countMap = new HashMap<>();
+  private final Map<String, Integer> vehicleTypeToFees = Map.of("car", 20, "truck", 50, "bike", 10);
 
-  public VehicleCounter(String name) {  super(name);  }
+  private Integer totalFees;
+
+  public VehicleCounter(String name) {
+    super(name);
+    this.totalFees = 0;
+  }
 
   @Override
   public void apply(Event vehicleEvent, List<Event> eventCollector) {
     String vehicle = ((VehicleEvent)vehicleEvent).getData();
     Integer count = countMap.getOrDefault(vehicle, 0) + 1;
     countMap.put(vehicle, count);
+    this.totalFees += vehicleTypeToFees.get(vehicle);
 
     System.out.println("VehicleCounter --> ");
     printCountMap();
+    printTotalFees();
   }
 
   private void printCountMap() {
@@ -31,5 +39,9 @@ class VehicleCounter extends Operator {
     for (String vehicle: vehicles) {
       System.out.println("  " + vehicle + ": " + countMap.get(vehicle));
     }
+  }
+
+  private void printTotalFees(){
+    System.out.printf("Total fees: %d\n", this.totalFees);
   }
 }
